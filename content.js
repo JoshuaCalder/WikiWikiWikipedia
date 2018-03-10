0cfe94dea69933c1ee38e50420787e091b43c5e4
Josh <joshuamcalder@hotmail.com>
2018-03-10 12:18:20 -0800

    get title from article

--- content.js
+++ content.js
@@ -1,7 +1,9 @@
 //content.js
+
+// changing the logo to a turntable
 turntable = "\"" + chrome.extension.getURL("icon.png") + "\"";
-console.log(turntable);
 $(".mw-wiki-logo").css("background-image", "url(" +turntable + ")");
+$(".mw-wiki-logo").text("WikiWikiWikipedia");
 
 // helper method that rotates the turntable icon
 $.fn.animateRotate = function(angle, duration, easing, complete) {
@@ -21,4 +23,16 @@ $.fn.animateRotate = function(angle, duration, easing, complete) {
   });
 };
 
-$('.mw-wiki-logo').animateRotate(1080, 10000);
+// prevents page redirects on logo click
+$('.mw-wiki-logo').click(function(e) {
+    e.preventDefault();
+});
+
+// rotates the turntable 2160 degrees for a duration of 10000 milliseconds
+$('.mw-wiki-logo').animateRotate(2160, 10000);
+
+// grabs the title of the Wikipedia article and trims out the '- Wikipedia part'
+title = document.title
+title = title.substring(0, title.length-12);
+
+
