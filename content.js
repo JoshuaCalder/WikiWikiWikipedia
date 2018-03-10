9ec4de75726a808e3e47c846d3c757cb027c5012
Josh <joshuamcalder@hotmail.com>
2018-03-10 11:40:06 -0800

    Changed wikipedia logo to turntable and added animation

--- content.js
+++ content.js
@@ -1,2 +1,24 @@
 //content.js
+turntable = "\"" + chrome.extension.getURL("icon.png") + "\"";
+console.log(turntable);
+$(".mw-wiki-logo").css("background-image", "url(" +turntable + ")");
 
+// helper method that rotates the turntable icon
+$.fn.animateRotate = function(angle, duration, easing, complete) {
+  return this.each(function() {
+    var $elem = $(this);
+
+    $({deg: 0}).animate({deg: angle}, {
+      duration: duration,
+      easing: easing,
+      step: function(now) {
+        $elem.css({
+           transform: 'rotate(' + now + 'deg)'
+         });
+      },
+      complete: complete || $.noop
+    });
+  });
+};
+
+$('.mw-wiki-logo').animateRotate(1080, 10000);
