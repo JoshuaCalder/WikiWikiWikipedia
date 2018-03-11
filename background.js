19278ca6635b898dc4d1922fc57b7cae5a05ea8b
avesk <avery@appreciationengine.com>
2018-03-10 16:56:52 -0800

    trying to import external gapi script in background.js

--- background.js
+++ background.js
@@ -1,8 +1,39 @@
 //background.js
 // chrome.browserAction.onClicked.addListener(function(tab) {
+//     debugger;
 //     chrome.tabs.executeScript(null,
 //      {file:"https://apis.google.com/js/client.js?onload=googleApiClientReady"});
 // });
+
+// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//     $.get("https://apis.google.com/js/client.js?onload=googleApiClientReady", function(result) {
+//         chrome.tabs.executeScript(tabs[0].id, {code: result});
+//     }, "text");
+// });
 //
-// chrome.browserAction.onClicked.addListener(function(tab) { alert("Loading api..."); });
-// search();
+
+// xhr = new XMLHttpRequest();
+// xhr.open("GET", "https://apis.google.com/js/client.js?onload=googleApiClientReady", true);
+// xhr.onreadystatechange = function() {
+//   if (xhr.readyState == 4) {
+//       // WARNING! Might be evaluating an evil script!
+//       // resp = eval("(" + xhr.responseText + ")");
+//       // Or this if it's work
+//       chrome.tabs.executeScript(tabs[0].id, {code: xhr.responseText});
+//   }
+// }
+// xhr.send();
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://apis.google.com/js/client.js?onload=googleApiClientReady", true);
+xhr.onreadystatechange = function() {
+  if (xhr.readyState == 4) {
+    // innerText does not let the attacker inject HTML elements.
+    // document.getElementById("resp").innerText = xhr.responseText;
+  }
+}
+xhr.send();
+
+chrome.browserAction.onClicked.addListener(function(tab) { alert("Loading api..."); });
+
+search();
